0baac046b96ef5c8c702b88b322c2d2ab485a938
loubishop <159189281+loubishop@users.noreply.github.com>
2024-02-07 13:14:36 -0600

    Add files via upload

--- customFunctions.js
+++ customFunctions.js
@@ -1,82 +1,63 @@
+let nightScene = false;
+let dayScene = false;
+let triangleBool = false;
 
-let squareBrushBool = false
-let ellipseBrushBool = false
-let triangleBool = false
-
-function setup(){
-
-	createCanvas(windowWidth, windowHeight)
-	//background(200, 50, 100)
-	rectMode(CENTER)
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    rectMode(CENTER);
+}
+function draw() {
+    if (drawnightScene) {
+        nightScene();
+    }
+    if (drawdayScene) {
+        dayScene();
+    }
+    if (drawtriangleBool) {
+        triangletracker();
+    }
 }
 
-function draw(){
-
-	if(squareBrushBool == true){
-		nightScene()
-	}
-	if(ellipseBrushBool == true){
-		ellipseBrush()
-	}
-	if(triangleBool== true){
-		triangletracker()
-	}
+// Custom brush function
+function drawnightScene() {
+    background(0);
+    fill(52, 220, 235);
+    ellipse(windowWidth / 2, windowHeight / 2, 100, 100);
 }
 
-
-//custom brush function
-function nightScene(){
-	// if(mouseIsPressed){
-	// 	fill(100, 250, 80)
-	// 	rect(mouseX, mouseY, 30, 30)
-	// }
-	background(0)
-	fill(52, 220, 235)
-	ellipse(windowWidth/2, windowHeight/2, 100, 100)
+function drawdayScene() {
+    background(50, 100, 200);
+    fill(235, 168, 52);
+    ellipse(windowWidth / 2, windowHeight / 2, 100, 100);
 }
 
-
-function ellipseBrush(){
-
-	background(50, 100, 200)
-	fill(235, 168, 52)
-	ellipse(windowWidth/2, windowHeight/2, 100, 100)
-	// if(mouseIsPressed){
-	// 	fill(80, 100, 200) 
-	// 	ellipse(mouseX, mouseY, 50, 50)
-	// }
+function drawtriangletracker() {
+    background(138, 39, 30);
+    fill(242, 94, 198);
+    triangle(mouseX, mouseY, windowWidth / 2, windowHeight / 2, 200, 700);
 }
 
-function triangletracker (){
-	background(138, 39, 30)
-	fill(242, 94, 198)
-	triangle(mouseX,mouseY,windowWidth/2,windowHeight/2,200,700)
+// p5 key pressed function
+function keyPressed() {
+    if (key === 'q') {
+        nightScene = true;
+        dayScene = false;
+        triangleBool = false;
+    }
+
+    if (key === 'w') {
+        nightScene = false;
+        dayScene = true;
+        triangleBool = false;
+    }
+
+    if (key === 'e') {
+        nightScene = false;
+        dayScene = false;
+        triangleBool = true;
+    }
 }
 
-
-//p5 key pressed function
-
-function keyPressed(){
-	if(key === 'q'){
-		squareBrushBool = true
-		ellipseBrushBool = false
-		triangleBool= false
-	}
-
-	if(key === 'w'){
-		squareBrushBool = false
-		ellipseBrushBool = true
-		triangleBool= false
-	}
-	if (key==='e'){
-		squareBrushBool = false
-		ellipseBrushBool = false
-		triangleBool= true
-
-	}
-
-
-}
 
 
 
